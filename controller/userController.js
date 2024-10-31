6bb8ac87970dcd472950ca92e0b0703fbf4b34b7
Manish Shetty <manishshetty9b@gmail.com>
2024-10-31 22:44:55 +0530

    added signup function

--- controller/userController.js
+++ controller/userController.js
@@ -11,7 +11,49 @@ require("dotenv").config()
 const JWT_SECRET = (process.env.JWT_SECRET)
 
 const userSignup = async (req, res) => {
+    try {
+        const parsedData = signUpValidation.safeParse(req.body)
+        if (!parsedData.success) {
+            return res.json({
+                message: "Incorrect format",
+                error: parsedData.error
+            })
+
+        }
+
+        const {userName, name, email, password} =req.body
+    }
+    catch (error) {
+        res.status(500).json({ message: "Something went Wrong" })
+        console.log(error)
+    }
+
+    try {
+    const hashedPassword = await bcrypt.hash(password, 5)
+}
+catch(e){
+    res.status(500).json({
+        message: "error while hashing"
+    })
 }
 
-const userSignin = async ( req ,res) =>
+    try {
+        await UserModel.create({
+            email: email,
+            password: hashedPassword,
+            name: name
+        });
+
+        res.status(200).json({
+            message: "You are signed up"
+        })
+
+    } catch (e) {
+        res.status(500).json({
+            message: "User already exists"
+        })
+    }
+}
+
+
 
